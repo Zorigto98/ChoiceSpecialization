8ee721789fae34adb733b7c46adee029d8aa3020
zorigto <gurzhapov98@mail.ru>
2022-11-28 00:08:25 +0800

    add prog

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿string [] addNewItem(string [] array, string item)
+{
+    if (array.Length == 0)
+    {
+        string [] result = new string [1] {item};
+        return result;
+    }
+    else 
+    {
+        string [] result = new string [array.Length + 1];
+        array.CopyTo(result, 0);
+        result [array.Length] = item;
+        return result;
+    }
+    
+}
+
+string [] str = {"hello" , "2", "world", ":-)", "23"};
+string [] str1 = {};
+for (int i = 0; i < str.Length; i++)
+{
+    if (str[i].Length <= 3) str1 = addNewItem(str1, str[i]);
+}
+Console.WriteLine($"Lenght {str1.Length}");
+Console.WriteLine("Result array: ");
+
+for ( int i = 0; i < str1.Length; i++)
+{
+    Console.Write($"{str1[i]} ");
+}
